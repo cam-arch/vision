a81f316a1042bf9da5353e9ab94945fa656ea8fb
cam-arch <40884189+cam-arch@users.noreply.github.com>
2023-02-21 21:05:12 +0000

    Working

--- Task 1/Task_1.py
+++ Task 1/Task_1.py
@@ -8,6 +8,45 @@ images = []
 detected_edges_images = []
 idx = 1
 
+def findTwoLines(lines):
+
+    maxDiff = 0
+    maxDiffPos = 0
+    cumRho1 = 0
+    cumRho2 = 0
+    cumTheta1 = 0
+    cumTheta2 = 0
+
+    sortedLines = sorted(lines, key=lambda x: x[0][0])
+
+    for idx in range(len(sortedLines) - 1):
+
+        if (sortedLines[idx + 1][0][0] - sortedLines[idx][0][0]) > maxDiff:
+
+            maxDiffPos = idx + 1
+            maxDiff = sortedLines[idx + 1][0][0] - sortedLines[idx][0][0]
+
+    for jdx in range (0, maxDiffPos):
+
+        cumRho1 += sortedLines[jdx][0][0]
+        cumTheta1 += sortedLines[jdx][0][1]
+
+    cumRho1 /= maxDiffPos
+    cumTheta1 /= maxDiffPos
+
+    for kdx in range (maxDiffPos, len(sortedLines)):
+        
+        cumRho2 += sortedLines[kdx][0][0]
+        cumTheta2 += sortedLines[kdx][0][1]
+
+    cumRho2 /= len(sortedLines) - maxDiffPos
+    cumTheta2 /= len(sortedLines) - maxDiffPos
+
+    twoLines = [[cumRho1, cumTheta1], [cumRho2, cumTheta2]]
+    
+    return twoLines
+
+
 for image in sorted(os.listdir('./angle')):
 
     imageO = cv2.imread('./angle/' + image, cv2.IMREAD_GRAYSCALE)
@@ -32,14 +71,13 @@ for image in sorted(os.listdir('./angle')):
     # # Copy edges to the images that will display the results in BGR
     cdst = cv2.cvtColor(imageO, cv2.COLOR_GRAY2BGR)
     
-    lines = cv2.HoughLines(blackAndWhiteImage, 1.6, np.pi / 180, 150, None, 0, 0)
+    lines = cv2.HoughLines(blackAndWhiteImage, 1, np.pi / 155, 120, None, 0, 0)
     
     if lines is not None:
-        for idx in range(0, len(lines)):
-
-            rho = lines[idx][0][0]
-            theta = lines[idx][0][1]
-            #print ((theta * 360) / (2 * math.pi))
+        twoLines = findTwoLines(lines)
+        for idx in range(2):
+            rho = twoLines[idx][0]
+            theta = twoLines[idx][1]
             a = math.cos(theta)
             b = math.sin(theta)
             x0 = a * rho
@@ -47,23 +85,19 @@ for image in sorted(os.listdir('./angle')):
             pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
             pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
             cv2.line(cdst, pt1, pt2, (0,0,255), 3, cv2.LINE_AA)
-        print (len(lines))
-    else:
-        print (0)
+
+    print("#######")
 
     cv2.imshow('Image with Edge Detection', cdst)
-
-    # # cv2.imwrite()
-
-    # # plt.imshow(cv2.cvtColor(cdst, cv2.COLOR_GRAY2RGB))
-
-    # # plt.savefig('./images_detected_edges/image_' + str(idx))
 
     cv2.waitKey(0)
     
     cv2.destroyAllWindows()
 
     idx += 1
+
+
+    
 
 
 
