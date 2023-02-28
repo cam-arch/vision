4ec6948d9f4b3d8b668c9b3a615a3c51c91b34be
LouieMiddleHawkeye <43111442+LouieMiddle@users.noreply.github.com>
2023-02-28 20:16:13 +0000

    task 2 accuracy and improving parts of caching

--- Task 2/Task_2.py
+++ Task 2/Task_2.py
@@ -1,132 +1,154 @@
+import json
 import os
+import timeit
+import re
 
 import cv2 as cv
 import natsort
-import json
 import numpy as np
 
+
 def image_pyramid(file, file_name, height, width):
-    ## Get image
+    # Get image
     src = cv.imread(cv.samples.findFile(file))
 
-    ## Scale image down
-    rows, cols, _channels = map(int, src.shape)         # get current image size
-    while rows != 32 and cols != 32:
+    # Scale image down
+    rows, cols, _channels = map(int, src.shape)  # get current image size
+    while True:
         rows //= 2
         cols //= 2
-        src = cv.pyrDown(src, dstsize=(rows, cols))     # scale image down
+        src = cv.pyrDown(src, dstsize=(rows, cols))  # scale image down
 
-        if (rows == height and cols == width): break    # correct size found; break from loop
+        # correct size found, break from loop
+        if rows == height and cols == width:
+            break
 
-    ## Give image black background
+    # Give image black background
     (threshold, black_and_white_image) = cv.threshold(src, 127, 255, cv.THRESH_BINARY)
 
-    return black_and_white_image, file_name
+    name = format_name(file_name)
+    return black_and_white_image, name
 
 
-def create_cache_templates(training_path, cache_path):
-    ## Create folder for cached templates
-    if os.path.exists(cache_path):
-        print("Cache already exists. Using old cache data.")
-        return
-    else:
-        os.makedirs(cache_path)
-    
-    ## Create files for each template
-    scales = [64, 128]      # The differenet scales we want to use
-    rotations = [45, 90, 135, 180, 225, 270, 315]   # The rotations of the templates
+def format_name(name):
+    name = name[4:]
+    return name[:-4]
+
+
+def get_templates(training_path, cache_path, use_cache):
+    # Create folder for cached templates
+    if os.path.exists(cache_path) and use_cache:
+        print("Cache already exists. Using existing cache data.")
+        return read_cache_templates(cache_path)
+
+    # Create files for each template
+    scales = [64]  # The different scales we want to use
+    rotations = []  # The rotations of the templates
+    templates = {}
     for image in natsort.natsorted(os.listdir(training_path), reverse=False):
-        templates = {}
-
-        ## Create scaled templates and add them to the dictionary
-        scaled_templates = []
         for scale in scales:
             new_template, name = image_pyramid(training_path + "/" + image, image, scale, scale)
-            scaled_templates.append(new_template)
-            templates[name + str(scale)] = new_template.tolist()
+            templates[name + "_" + str(scale) + "_" + "0"] = new_template
+            # Create rotated templates and add them to the dictionary
+            for rotation in rotations:
+                _, w, h = new_template.shape[::-1]
+                rotation_angle = cv.getRotationMatrix2D((w / 2, h / 2), rotation, 1)  # Create rotation matrix
+                new_template = cv.warpAffine(new_template, rotation_angle, (w, h))  # Rotate image
+                templates[name + "_" + str(scale) + "_" + str(
+                    rotation)] = new_template  # Add image to dictionary. tolist() needed for json serializing
 
-        ## Create rotated templates and add them to the dictionary
-        for rotation in rotations:
-            for scaled_template in scaled_templates:
-                _, w, h = scaled_template.shape[::-1]
-                rotation_angle = cv.getRotationMatrix2D((w/2, h/2), rotation, 1)            # Create rotation matrix
-                new_template = cv.warpAffine(scaled_template, rotation_angle, (w, h))       # Rotate image
-                templates[name + str(rotation)] = new_template.tolist()                                 # Add image to dictionary. tolist() needed for json serializing
-
-        ## Add dictionary to file
-        with open(cache_path + "/" + str(image).replace("png", "txt"), 'w') as f:
+    if use_cache:
+        os.makedirs(cache_path)
+        # Add dictionary to file
+        with open(cache_path + "/cache.txt", 'w') as f:
             json.dump(dict(templates), f)
-        f.close()
+            f.close()
+            print("\nCached templates successfully.")
 
-    print("\nCached all templates successfuly.")
-
-
-def read_cache_templates(cache_path):
-    templates = []
-    for filename in natsort.natsorted(os.listdir(cache_path), reverse=False):
-        ## Read file
-        with open(os.path.join(cache_path, filename), 'r') as f:
-            data = json.load(f)
-        
-        ## Convert dictionary of images to one long list of images
-        images = list(data.values())
-        for image in images:
-            image = np.array(image)             # Convert list to numpy.
-            image = image.astype(np.uint8)      # Convert numpy dtype to 8-bit. Needed for opencv
-            templates.append((image, filename[:-3]))
-
-    print("Read cache successfuly.")
     return templates
 
 
-def run_task_2(training_path, test_path, cache_path, use_cache):
-    ## Test path because of Laurence's laptop
-    x = os.listdir(training_path)
+def read_cache_templates(cache_path):
+    # Read file
+    with open(os.path.join(cache_path, "cache.txt"), 'r') as f:
+        data = json.load(f)
 
-    ## Create templates
-    templates = []
-    if (use_cache):
-        create_cache_templates(training_path, cache_path)      # Create cache templates
-        templates = read_cache_templates(cache_path)        # Read cache templates
-    else:
-        for image in natsort.natsorted(os.listdir(training_path), reverse=False):
-            template = image_pyramid(training_path + "/" + image, image, 64, 64)
-            templates.append(template)
-   
+        print("Read cache successfully.")
+        return data
 
-    ## Find templates in images
-    templates_found = []
+
+def accuracy(test_annotations_path, templates_found):
+    accuracy_ = {}
+    annotation_pattern = r"^([a-zA-Z-]+), \((\d+), (\d+)\), \((\d+), (\d+)\)$"
+    for image, template_found in templates_found.items():
+        template_found = [(x[0].split('_')[0], x[1], x[2]) for x in template_found]
+        image = image.replace(".png", ".txt")
+
+        with open(os.path.join(test_annotations_path, image), "r") as f:
+            lines = f.readlines()
+            correct_template = []
+            for i, line in enumerate(lines):
+                match = re.match(annotation_pattern, line)
+                if match:
+                    result = (match.group(1), (int(match.group(2)), int(match.group(3))),
+                              (int(match.group(4)), int(match.group(5))))
+                    correct_template.append(result)
+
+        # TODO: Split out if correct template found, and how close
+        #  Euclidean distance of centre of boxes to determine
+        #  Cam's method of how accurate (how much the bboxes overlap)
+        correct, incorrect = 0, 0
+        for template in template_found:
+            if template in correct_template:
+                correct += 1
+            else:
+                incorrect += 1
+
+        accuracy_[image] = (correct, incorrect)
+
+    return accuracy_
+
+
+def run_task_2(training_path, test_path, test_annotations_path, cache_path, use_cache):
+    # Create templates
+    templates = get_templates(training_path, cache_path, use_cache)
+
+    # Find templates in images
+    templates_found = {}
     for image in natsort.natsorted(os.listdir(test_path), reverse=False):
         img_rgb = cv.imread(test_path + "/" + image)
         (threshold, black_and_white_image) = cv.threshold(img_rgb, 127, 255, cv.THRESH_BINARY)
 
-        templates_position = []
-        for template, name in templates:
+        templates_found[image] = []
+        for name, template in templates.items():
+            # TODO: Come back to cache
+            # template = np.reshape(template, (32, 32, 3)).astype(np.uint8)
             _, w, h = template.shape[::-1]
             res = cv.matchTemplate(black_and_white_image, template, cv.TM_CCOEFF_NORMED)
 
-            threshold = 0.9     # Making this 0.95 did not work for me
+            threshold = 0.9  # Making this 0.95 did not work for me
             loc = np.where(res >= threshold)
             if len(loc[0]) != 0:
                 # TODO: This can sometimes be 1 pixel off
-                #  Also template has to be 64 x 64
                 y_min = round(np.average(loc[0]))
                 x_min = round(np.average(loc[1]))
                 y_max = y_min + w
                 x_max = x_min + h
-                templates_position.append((name, (x_min, y_min), (x_max, y_max)))
+                templates_found[image].append((name, (x_min, y_min), (x_max, y_max)))
             for pt in zip(*loc[::-1]):
                 cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
 
-        templates_found.append(templates_position)
         # cv.imshow('bbox', img_rgb)
         # cv.waitKey(0)
         print("\nChecked image: " + image)
         print(templates_found)
 
-    ## Print output
-    for images_templates in templates_found:
-        print(images_templates)
+    # for template_found in templates_found:
+    #     print(template_found)
+
+    accuracy_ = accuracy(test_annotations_path, templates_found)
+    print("")
+    print(accuracy_)
 
     cv.destroyAllWindows()
 
@@ -134,11 +156,11 @@ def run_task_2(training_path, test_path, cache_path, use_cache):
 if __name__ == "__main__":
     training_path = "./Task2Dataset/Training/png"
     test_path = "./Task2Dataset/TestWithoutRotations/images"
-    cache_path = training_path.replace("png", "cache")      # "./Task2Dataset/Training/cache"
-    use_cache = True
+    cache_path = training_path.replace("png", "cache")  # "./Task2Dataset/Training/cache"
+    test_annotations_path = "./Task2Dataset/TestWithoutRotations/annotations"
+    use_cache = False
 
-    try:    
-        run_task_2(training_path, test_path, cache_path, use_cache)
-    except:
-        print("Laurence's laptop")
-        run_task_2(training_path.replace(".", "Task 2"), test_path.replace(".", "Task 2"), cache_path.replace(".", "Task 2"), use_cache)
+    start = timeit.default_timer()
+    run_task_2(training_path, test_path, test_annotations_path, cache_path, use_cache)
+    stop = timeit.default_timer()
+    print("Run-time: ", stop - start)
